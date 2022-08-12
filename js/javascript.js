477ae71ba324c9356826f2822c9ee45b7410fa21
Jinoon Choi <jinoon.choi@gmail.com>
2022-08-12 23:38:33 +0900

    tset

--- js/javascript.js
+++ js/javascript.js
@@ -82,12 +82,12 @@ function preloadPages() {
     prePages = pageNum;
   }
 
-    for (let i = prePages - 1; i < lastPage + 7; i++) {
-      let pages = new Image();
-      pages.src = pageUrl + i + ".webp";
-      console.log("load pages");
-      console.log(i);
-    }
+  for (let i = prePages - 1; i < prePages + 7; i++) {
+    let pages = new Image();
+    pages.src = pageUrl + i + ".webp";
+    console.log("load pages");
+    console.log(i);
+  }
 }
 
 let firstPageKo = new Image();
